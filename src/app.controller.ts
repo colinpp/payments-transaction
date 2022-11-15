fcb4718252ffeb9a3676cc650ccfd7b9fd01fb6a
colinpp <colinpp1@gmail.com>
2022-11-15 14:47:56 +0200

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @MessagePattern({ cmd: 'pay' })
+  pay(data: any){
+    return this.appService.pay(data);
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern({ cmd: 'all' })
+  all(data: any){
+    return this.appService.all();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Payment } from './payment.entity';
+
+import {config} from './orm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(config),
+    TypeOrmModule.forFeature([Payment]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
